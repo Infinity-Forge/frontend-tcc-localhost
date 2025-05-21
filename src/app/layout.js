1b4f420ac802ca966466562912d47333c33c6c37
Vinicius Dadalte <viniciusdadalte745@email.com>
2025-05-21 14:22:25 -0300

    Arrumei a estrutura do projeto e ja adicionei os diretorios que vamos precisar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./reset.css";
-import "./globals.css";
+import "../styles/reset.css";
+import "../styles/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -19,7 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
