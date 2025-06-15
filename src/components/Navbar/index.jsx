b0d353801e61a5db6106d2e2e285a4ea2465c81e
EvandroOrso <evandrofc15@gmail.com>
2025-06-15 16:34:05 -0300

    Arrumei o position da navbar

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -11,34 +11,67 @@ function Navbar({ children }) {
     const [toggleSuporte, setToggleSuporte] = useState(false);
 
     function handleToggleMenu() {
-        setToggleMenu(prevToggleMenu => !prevToggleMenu);
+        setToggleMenu((prevToggleMenu) => !prevToggleMenu);
     }
 
     function handleToggleSuporte() {
-        setToggleSuporte(prevToggleSuporte => !prevToggleSuporte);
+        setToggleSuporte((prevToggleSuporte) => !prevToggleSuporte);
     }
 
-    return(
-        <header>
+    return (
+        <header className={styles.siteHeader}>
             <nav className={styles.nav}>
-                <Image height={40} width={65} src="/gd.png" alt="Logo do jogo" id="logoNavbar"/>
-                <button className={styles.hamburger} onClick={handleToggleMenu}>&#9776;</button>
-                <div className={styles.dropdown}>
-                    <button className={styles.navbarButton}>Informações do Jogo</button>
-                    <div className={styles["dropdown-content"]}>
-                        <Link className={styles.navLink} href="/personagens">Personagens</Link>
-                        <Link className={styles.navLink} href="/mapas">Mapas</Link>
-                        <Link className={styles.navLink} href="/arsenal">Arsenal</Link>
+                <Image height={40} width={65} src="/gd.png" alt="Logo do jogo" id="logoNavbar" />
+                
+                {/* Botão que controla o menu de hamburguer */}
+                <button className={styles.hamburger} onClick={handleToggleMenu}>
+                    &#9776;
+                </button>
+
+                {/* Menu de hamburguer que expande ou diminui */}
+                <div className={`${styles.menu} ${toggleMenu ? styles.open : ''}`}>
+                    <div className={styles.dropdown}>
+                        <button className={styles.navbarButton}>
+                            Informações do Jogo
+                        </button>
+                        <div className={styles["dropdown-content"]}>
+                            <Link className={styles.navLink} onClick={handleToggleMenu} href="/personagens">
+                                Personagens
+                            </Link>
+                            <Link className={styles.navLink} onClick={handleToggleMenu} href="/mapas">
+                                Mapas
+                            </Link>
+                            <Link className={styles.navLink} onClick={handleToggleMenu} href="/arsenal">
+                                Arsenal
+                            </Link>
+                        </div>
                     </div>
+
+                    <Link href="/noticias" onClick={handleToggleMenu}>
+                        <button className={styles.navbarButton}>
+                            Notícias
+                        </button>
+                    </Link>
+
+                    <a
+                        href="#"
+                        onClick={(e) => {
+                            e.preventDefault();
+                            handleToggleSuporte();
+                            handleToggleMenu();
+                        }}
+                    >
+                        <button className={styles.navbarButton}>
+                            Suporte
+                        </button>
+                    </a>
+
+                    {children}
+
+                    <button id={styles.jogar}>
+                        Jogar Agora
+                    </button>
                 </div>
-                <Link href="/noticias">
-                    <button className={styles.navbarButton}>Notícias</button>
-                </Link>
-                <a href="#">
-                    <button className={styles.navbarButton} onClick={handleToggleSuporte}>Suporte</button>
-                </a>
-                {children}
-                <button id={styles.jogar}>Jogar Agora</button>
             </nav>
         </header>
     )
