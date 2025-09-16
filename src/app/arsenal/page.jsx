1b17c42d71d05f5b157b99aa3c52b17217af1b2c
EvandroOr <evandroetecemail@gmail.com>
2025-09-16 11:19:18 -0300

    Adição de rotas relativas para o arsenal

--- src/app/arsenal/page.jsx
+++ src/app/arsenal/page.jsx
@@ -9,43 +9,51 @@ import styles from "./page.module.css";
 
 function Arsenal() {
   const armas = {
-  pistolas: [
+    pistolas: [
       {
+        id: 1,
         nome: "Pistola",
         src: "/arma.png",
         alt: "Pistola"
       },
       {
+        id: 2,
         nome: "Pistola",
         src: "/arma.png",
         alt: "Pistola"
       },
       {
+        id: 3,
         nome: "Pistola",
         src: "/arma.png",
         alt: "Pistola"
       },
       {
+        id: 4,
         nome: "Pistola",
         src: "/arma.png",
         alt: "Pistola"
       },
       {
+        id: 5,
         nome: "Pistola",
         src: "/arma.png",
         alt: "Pistola"
       },
       {
+        id: 6,
         nome: "Pistola",
         src: "/arma.png",
         alt: "Pistola"
       },
       {
+        id: 7,
         nome: "Pistola",
         src: "/arma.png",
         alt: "Pistola"
       },
       {
+        id: 8,
         nome: "Pistola",
         src: "/arma.png",
         alt: "Pistola"
@@ -53,41 +61,49 @@ function Arsenal() {
     ],
     facas: [
       {
+        id: 9,
         nome: "Faca",
         src: "/arma.png",
         alt: "Faca"
       },
       {
+        id: 10,
         nome: "Faca",
         src: "/arma.png",
         alt: "Faca"
       },
       {
+        id: 11,
         nome: "Faca",
         src: "/arma.png",
         alt: "Faca"
       },
       {
+        id: 12,
         nome: "Faca",
         src: "/arma.png",
         alt: "Faca"
       },
       {
+        id: 13,
         nome: "Faca",
         src: "/arma.png",
         alt: "Faca"
       },
       {
+        id: 14,
         nome: "Faca",
         src: "/arma.png",
         alt: "Faca"
       },
       {
+        id: 15,
         nome: "Faca",
         src: "/arma.png",
         alt: "Faca"
       },
       {
+        id: 16,
         nome: "Faca",
         src: "/arma.png",
         alt: "Faca"
@@ -95,41 +111,49 @@ function Arsenal() {
     ],
     riffles: [
       {
+        id: 17,
         nome: "Riffle",
         src: "/arma.png",
         alt: "Riffle"
       },
       {
+        id: 18,
         nome: "Riffle",
         src: "/arma.png",
         alt: "Riffle"
       },
       {
+        id: 19,
         nome: "Riffle",
         src: "/arma.png",
         alt: "Riffle"
       },
       {
+        id: 20,
         nome: "Riffle",
         src: "/arma.png",
         alt: "Riffle"
       },
       {
+        id: 21,
         nome: "Riffle",
         src: "/arma.png",
         alt: "Riffle"
       },
       {
+        id: 22,
         nome: "Riffle",
         src: "/arma.png",
         alt: "Riffle"
       },
       {
+        id: 23,
         nome: "Riffle",
         src: "/arma.png",
         alt: "Riffle"
       },
       {
+        id: 24,
         nome: "Riffle",
         src: "/arma.png",
         alt: "Riffle"
@@ -148,15 +172,15 @@ function Arsenal() {
         <section className="armas">
           <Title text="Pistola" marginTopValue="86px"/>
           <ContainerPersonagens className={styles.containerPersonagem}>
-            {armas.pistolas.map((pistola, index) => <CardArma key={index} nome={pistola.nome} src={pistola.src} alt={pistola.alt}/>)}
+            {armas.pistolas.map((pistola, index) => <CardArma key={index} id={pistola.id} nome={pistola.nome} src={pistola.src} alt={pistola.alt}/>)}
           </ContainerPersonagens>
           <Title text="Faca"   marginTopValue="48px"/>
           <ContainerPersonagens className={styles.containerPersonagem}>
-            {armas.facas.map((faca, index) => <CardArma key={index} nome={faca.nome} src={faca.src} alt={faca.alt}/>)}
+            {armas.facas.map((faca, index) => <CardArma key={index} id={faca.id} nome={faca.nome} src={faca.src} alt={faca.alt}/>)}
           </ContainerPersonagens>
           <Title text="Riffle" marginTopValue="48px"/>
           <ContainerPersonagens className={styles.containerPersonagem}>
-            {armas.riffles.map((riffle, index) => <CardArma key={index} nome={riffle.nome} src={riffle.src} alt={riffle.alt}/>)}
+            {armas.riffles.map((riffle, index) => <CardArma key={index} id={riffle.id} nome={riffle.nome} src={riffle.src} alt={riffle.alt}/>)}
           </ContainerPersonagens>
         </section>
       </main>
--- src/components/ContainerPersonagens/CardArma/index.jsx
+++ src/components/ContainerPersonagens/CardArma/index.jsx
@@ -1,14 +1,12 @@
 import Link from "next/link";
 import styles from "./index.module.css";
 
-function CardArma({ src, alt, nome}) {
+function CardArma({ src, alt, nome, id }) {
     
     return(
-        <Link href="/arsenal/descricao">
-            <div className={styles.characterContainer}>
-                <img src={src} alt={alt} />
-                <div className={styles.characterName}>{nome}</div>
-            </div>
+        <Link href={`/arsenal/descricao/${id}`} className={styles.characterContainer}>
+            <img src={src} alt={alt} />
+            <div className={styles.characterName}>{nome}</div>
         </Link>
     )
 }
