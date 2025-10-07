cefddb51c85421e4730a4f2a308056526eb067f9
EvandroOr <evandroetecemail@gmail.com>
2025-10-07 10:41:35 -0300

    Pegando dados da API

--- src/app/personagens/page.jsx
+++ src/app/personagens/page.jsx
@@ -10,9 +10,41 @@ import ContainerPersonagens from "@/components/ContainerPersonagens";
 import CardPersonagem from "@/components/ContainerPersonagens/CardPersonagem";
 import Footer from "@/components/Footer";
 import styles from "./page.module.css"
-import { personagens } from "@/simulacaoDeDados";
+import api from "../../services/api";
 
 function Personagens() {
+
+  const [personagens, setPersonagens] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    listarPersonagens();
+  }, []);
+
+  async function listarPersonagens() {
+    try {
+      setLoading(true);
+      const response = await api.get('/personagens/');
+
+      if (response.data.sucesso == true) {
+          const personagemApi = response.data.dados;
+          setPersonagens(personagemApi);
+      } else {
+          alert(`Erro ${error.responde.data.message} \n ${error.response.data.dados}`);
+      }
+      
+    } catch (error) {
+
+      if (error.response) {
+          alert(error.response.data.mensagem + "\n" + error.response.data.dados);
+      } else {
+          alert("Erro no front-end" + "\n" + error);
+      }
+
+    } finally {
+        setLoading(false);
+    }
+  }
 
   return (
     <>
@@ -26,7 +58,15 @@ function Personagens() {
         <section className={styles.characters}>
           <Title text="Guardiões" className={styles.title}/>
           <ContainerPersonagens className={styles.container}>
-            {personagens.guardioes.map((guardiao, index) => <CardPersonagem key={index} rota={"personagens"} id={guardiao.id} nome={guardiao.nome} imageSrc={guardiao.src} alt={guardiao.alt}/>)}
+            {loading ? (
+                <div>Carregando...</div>
+            ) : personagens.length > 0 ? (
+              personagens.map((personagem) => (
+                  <CardPersonagem key={personagem.id} rota={"personagens"} id={personagem.id} nome={personagem.nome} imageSrc={personagem.src} alt={personagem.alt}/>
+              ))
+            ) : (
+                <h1>Não foi possível carregar os personagens</h1>
+            )}
           </ContainerPersonagens>
           <Title text="Cavaleiros" className={styles.title}/>
           <ContainerPersonagens className={styles.container}>
