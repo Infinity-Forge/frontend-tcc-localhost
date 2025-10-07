6dfc227fc0ecc7c5929df1fc3db3fdb73e2d8da1
EvandroOr <evandroetecemail@gmail.com>
2025-10-07 09:56:05 -0300

    Criação do .env.local

--- src/app/personagens/page.jsx
+++ src/app/personagens/page.jsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Link from "next/link";
+import { useState, useEffect } from "react";
 import Container from "@/components/Container";
 import Navbar from "@/components/Navbar";
 import CarrosselBanner from "@/components/CarrosselBanner";
